04aed7f4c7bee338bf57f79f19b02025a4306081
Jeremy Muñoz Torres <JMunoz@contractor.nexstardigital.com>
2021-11-06 18:04:08 -0400

    Added all method endpoints

--- technologies/serializers.py
+++ technologies/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from . import models
+import technologies
 
 
 class FeaturedCodeSerializer(serializers.ModelSerializer):
@@ -9,28 +10,46 @@ class FeaturedCodeSerializer(serializers.ModelSerializer):
 
 
 class TechnologySerializer(serializers.ModelSerializer):
-    featured_code = FeaturedCodeSerializer()
-
+    featured_code = FeaturedCodeSerializer(required=False)
+    resource_count = serializers.IntegerField()
 
     def create(self, validated_data):
         if validated_data.get('featured_code', None):
-            featured_code = models.FeaturedCode(**validated_data.get('featured_code'))
+            featured_code = models.FeaturedCode(
+                **validated_data.get('featured_code'))
             featured_code.save()
             validated_data['featured_code'] = featured_code
             technology = models.Technology(**validated_data)
             technology.save()
             return technology
-        return models.Technology(**validated_data).save()
+        else:
+            technology = models.Technology(**validated_data)
+            technology.save()
+            return technology
 
-
+    def update(self, instance, validated_data):
+        if validated_data.get('featured_code', None):
+            featured_code = models.FeaturedCode(
+                **validated_data.get('featured_code'))
+            featured_code.save()
+            validated_data['featured_code'] = featured_code
+            technology = models.Technology(**validated_data)
+            technology.save()
+            return technology
+        else:
+            instance = models.Technology(**validated_data)
+            instance.save()
+            return instance
 
     class Meta:
         model = models.Technology
         fields = ['id', 'title', 'featured_code',
-                  'description', 'cover_img', 'last_update']
+                  'description', 'cover_img', 'last_update', 'resource_count']
 
 
 class ResourceSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = models.Resource
-        fields = ['id', 'title', 'description', 'url', 'is_free', 'technology']
+        fields = ['id', 'title', 'description',
+                  'url', 'is_free', 'technology']
--- technologies/views.py
+++ technologies/views.py
@@ -1,6 +1,8 @@
+from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from . import models
 from technologies.serializers import ResourceSerializer, TechnologySerializer
@@ -16,21 +18,32 @@ def technologies_list(request):
     if request.method == 'GET':
         technologies = models.Technology.objects.select_related(
             'featured_code'
-        ).all()
+        ).annotate(resource_count=Count('resource')).all()
         serializer = TechnologySerializer(technologies, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = TechnologySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.validated_data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def technology_details(request, id):
-    technology = get_object_or_404(models.Technology, pk=id)
-    serializer = TechnologySerializer(technology)
-    return Response(serializer.data)
+    technology = get_object_or_404(models.Technology.objects.annotate(
+        resource_count=Count('resource')).all(), pk=id)
+
+    if request.method == 'GET':
+        serializer = TechnologySerializer(technology)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TechnologySerializer(technology, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        technology.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
@@ -40,15 +53,33 @@ def technology_resources(request, id):
     return Response(serializer.data)
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def resources_list(request):
-    resources = models.Resource.objects.all()
-    serializer = ResourceSerializer(resources, many=True)
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        resources = models.Resource.objects.all()
+        serializer = ResourceSerializer(resources, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        is_many = isinstance(request.data, list)
+        serializer = ResourceSerializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def resource_details(request, id):
-    technology = get_object_or_404(models.Resource, pk=id)
-    serializer = ResourceSerializer(technology)
-    return Response(serializer.data)
+    resource = get_object_or_404(models.Resource, pk=id)
+
+    if request.method == 'GET':
+        serializer = ResourceSerializer(resource)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ResourceSerializer(resource, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        resource.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
