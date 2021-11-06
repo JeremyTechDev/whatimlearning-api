ff771b59a31e7e2c1cd25af3fc8c94b82a9c9625
Jeremy Muñoz Torres <JMunoz@contractor.nexstardigital.com>
2021-11-05 21:25:22 -0400

    Added View Filters and Django Debug Toolbar

--- technologies/admin.py
+++ technologies/admin.py
@@ -5,23 +5,28 @@ from django.urls import reverse
 from . import models
 
 
-@admin.register(models.FeaturedCode)
-class FeaturedCodeAdmin(admin.ModelAdmin):
-    list_display = ['language', 'code']
-    search_fields = ['language__istartswith']
+class FeaturedCodeItemInline(admin.StackedInline):
+    model = models.FeaturedCode
+
+
+class ResourceItemInline(admin.StackedInline):
+    model = models.Resource
+    extra = 0
 
 
 @admin.register(models.Technology)
 class TechnologyAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'resources_count', 'last_update']
     search_fields = ['title__istartswith']
+    list_prefetch_related = ['resources']
+    inlines = [FeaturedCodeItemInline, ResourceItemInline]
 
     @admin.display(ordering='resources_count')
-    def resources_count(self, resource):
+    def resources_count(self, technology):
         url = (reverse('admin:technologies_resource_changelist')
                + '?'
-               + urlencode({'technology__id': str(resource.id)}))
-        return format_html('<a href="{}">{}</a>', url, resource.resources_count)
+               + urlencode({'technology__id': str(technology.id)}))
+        return format_html('<a href="{}">{}</a>', url, technology.resources_count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
@@ -31,5 +36,15 @@ class TechnologyAdmin(admin.ModelAdmin):
 
 @admin.register(models.Resource)
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description']
+    list_display = ['title', 'technology', 'url']
+    list_filter = ['is_free']
+    list_select_related = ['technology']
     search_fields = ['title__istartswith']
+
+
+@admin.register(models.FeaturedCode)
+class FeaturedCodeAdmin(admin.ModelAdmin):
+    list_display = ['language', 'code', 'technology']
+    list_filter = ['language']
+    list_select_related = ['technology']
+    search_fields = ['language__istartswith']
