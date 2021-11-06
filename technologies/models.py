d1fe159a4b4047a0a1b62445e85b73ab01197770
Jeremy Muñoz Torres <JMunoz@contractor.nexstardigital.com>
2021-11-06 16:23:40 -0400

    Fixed (again) relationship

--- technologies/models.py
+++ technologies/models.py
@@ -1,30 +1,6 @@
 from django.db import models
 
 
-class Technology(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(max_length=1000, null=True)
-    cover_img = models.URLField(null=True)
-    last_update = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name_plural = 'Technologies'
-
-    def __str__(self) -> str:
-        return self.title
-
-
-class Resource(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(max_length=1000, null=True)
-    url = models.URLField()
-    is_free = models.BooleanField(default=True)
-    technology = models.ForeignKey(Technology, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return self.title
-
-
 class FeaturedCode(models.Model):
     PLAIN_TEXT = 'TXT'
     JAVASCRIPT = 'JS'
@@ -47,7 +23,34 @@ class FeaturedCode(models.Model):
         choices=LANGUAGES_CHOICES,
         default=PLAIN_TEXT
     )
-    technology = models.OneToOneField(Technology, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"[{self.language}]: {self.code[:15]}..."
+
+
+class Technology(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000, null=True)
+    cover_img = models.URLField(null=True)
+    last_update = models.DateTimeField(auto_now=True)
+    featured_code = models.OneToOneField(
+        FeaturedCode,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name_plural = 'Technologies'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Resource(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000, null=True)
+    url = models.URLField()
+    is_free = models.BooleanField(default=True)
+    technology = models.ForeignKey(Technology, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
